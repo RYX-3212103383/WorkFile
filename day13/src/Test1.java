415522103f18768a13ad917d0069ea9addca1914
RYX <RYX18273144554@outlook.com>
2023-04-13 18:12:37 +0800

    day13

---
+++ day13/src/Test1.java
@@ -0,0 +1,197 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author RYX
+ */
+public class Test1 {
+	static Scanner input = new Scanner(System.in);
+	static Random r = new Random();
+
+	public static void main(String[] args) {
+		Test1 obj1 = new Test1();
+		System.out.println("作业1-10");
+		int a = input.nextInt();
+		switch (a) {
+			case 1: {
+				obj1.work1();
+				break;
+			}
+			case 2: {
+				obj1.work2();
+				break;
+			}
+			case 3: {
+				obj1.work3();
+				break;
+			}
+			case 4: {
+				obj1.work4();
+				break;
+			}
+			case 5: {
+				obj1.work5();
+				break;
+			}
+			case 6: {
+				obj1.work6();
+				break;
+			}
+			case 7: {
+				obj1.work7();
+				break;
+			}
+			case 8: {
+				obj1.work8();
+				break;
+			}
+			case 9: {
+				obj1.work9();
+				break;
+			}
+			case 10: {
+				obj1.work10();
+				break;
+			}
+			default:
+				System.out.println("1-10才有哦");
+		}
+	}
+
+	void work1() {
+		show();
+	}
+
+	public void show() {
+		System.out.println("战神居然是魔煞星^)^");
+	}
+
+	void work2() {
+		int a = 10, b = 15;
+		int max = max(a, b);
+		System.out.println("max=" + max);
+	}
+
+	public int max(int a, int b) {
+		return a > b ? a : b;
+	}
+
+	void work3() {
+		double a = 3.14, b = 1.14;
+		double min = min(a, b);
+		System.out.println("min=" + min);
+	}
+
+	public double min(double a, double b) {
+		return a < b ? a : b;
+	}
+
+	void work4() {
+		int year = 1965;
+		boolean leaper = isLeaper(year);
+		System.out.println(year + "是闰年？" + leaper);
+	}
+
+	public boolean isLeaper(int year) {
+		return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+	}
+
+	void work5() {
+		int cup = r.nextInt(3);
+		System.out.println(cup+"出拳 石头0，剪刀1，布2");
+		int you = input.nextInt();
+		int result = guessGame(you, cup);
+		if (result == 0) {
+			System.out.println("你赢了");
+		} else if (result == 1) {
+			System.out.println("你输了");
+		} else {
+			System.out.println("平局了");
+		}
+	}
+
+	public int guessGame(int you, int cup) {
+		if(you==cup){
+			return 2;
+		} else if (cup-you==1||(you==2&&cup==0)) {
+			return 0;
+		}else {
+			return 1;
+		}
+	}
+
+	void work6() {
+		float r= 3.2F;
+		float a=area(r);
+		System.out.println("面积为"+a);
+	}
+
+	private float area(float r) {
+		return (float) (3.14*r*r);
+	}
+
+	void work7() {
+		int[] a=new int[]{1,2,3,5,8,6,6,4,5,6,4,5,5,5};
+		showArray(a);
+	}
+
+	protected void showArray(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+"\t");
+		}
+	}
+
+	void work8() {
+		int[] a=new int[]{1,2,3,5,8,6,6,4,5,6,4,5,5,5};
+		int sum=sumArray(a);
+	}
+
+	public int sumArray(int[] a) {
+		int sum=0;
+		for (int i = 0; i < a.length; i++) {
+			sum+=a[i];
+		}
+		return sum;
+	}
+
+	void work9() {
+		int[][] a=new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		int[] b=sumDoubleArray(a);
+	}
+
+	public int[] sumDoubleArray(int[][] a) {
+		int[] b=new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			int rowsum=0;
+			for (int j = 0; j < a[i].length; j++) {
+				rowsum+=a[i][j];
+			}
+			b[i]=rowsum;
+		}
+		return b;
+	}
+
+	void work10() {
+		char[] a=getCode();
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+		}
+	}
+
+	public char[] getCode() {
+		String a="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		char[] b= new char[8];
+		for(int i=0;i<b.length;i++){
+			b[i]=a.charAt((int)(Math.random()*(a.length())));
+			for (int j = 0; j < i; j++) {
+				if(b[i]==b[j]){
+					b[i]='*';
+					i--;
+				}
+			}
+		}
+		return b;
+	}
+
+
+}
---
+++ day13/src/Test2.java
@@ -0,0 +1,106 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author RYX
+ */
+public class Test2 {
+	static Scanner input = new Scanner(System.in);
+	static Random r = new Random();
+
+	public static void main(String[] args) {
+		Test2 obj2 = new Test2();
+		System.out.println("作业1-5");
+		int a = input.nextInt();
+		switch (a) {
+			case 1: {
+				obj2.work1();
+				break;
+			}
+			case 2: {
+				obj2.work2();
+				break;
+			}
+			case 3: {
+				obj2.work3();
+				break;
+			}
+			case 4: {
+				obj2.work4();
+				break;
+			}
+			case 5: {
+				obj2.work5();
+				break;
+			}
+			default:
+				System.out.println("1-5才有哦");
+		}
+	}
+
+	void work1() {
+		int a = 10, b = 15;
+		int sum = sum(a, b);
+		System.out.println("sum=" + sum);
+	}
+
+	private int sum(int a, int b) {
+		return a + b;
+	}
+
+	void work2() {
+		int a = 15, b = 20;
+		int min = min(a, b);
+		System.out.println("min=" + min);
+	}
+
+	private int min(int a, int b) {
+		return a < b ? a : b;
+	}
+
+	void work3() {
+		int a = 10;
+		int result = factorial(a);
+		System.out.println(a + "阶乘为" + result);
+	}
+
+	private int factorial(int a) {
+		int result = 1;
+		for (int i = 0; i < a; i++) {
+			result *= i;
+		}
+		return result;
+	}
+
+	void work4() {
+		String s = "fsdfghjkl";
+		System.out.println(s);
+		String b = reversr(s);
+		System.out.println(b);
+	}
+
+	private String reversr(String s) {
+		StringBuilder a = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			a.append(s.charAt(s.length() - i - 1));
+		}
+		return a.toString();
+	}
+
+	void work5() {
+	int a=97;
+	boolean prime=isPrime(a);
+		System.out.println(a+"是质数"+prime);
+	}
+
+	private boolean isPrime(int a) {
+		boolean flag=true;
+		for (int i = 2; i < a; i++) {
+			if(a%i==0) {
+				flag=false;
+				return flag;
+			}
+		}
+		return flag;
+	}
+}
