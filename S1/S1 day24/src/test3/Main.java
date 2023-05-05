48ba67e55f84ccef2e50667f829c8f706cad6b8e
RYX <RYX18273144554@outlook.com>
2023-05-05 11:08:50 +0800

    day24

--- S1/S1 day24/src/test3/Main.java
+++ S1/S1 day24/src/test3/Main.java
@@ -9,12 +9,8 @@ public class Main {
 		String path = JOptionPane.showInputDialog(null, "请输入路径");
 		File file = new File(path);
 		if (!file.exists()) {
-			try {
-				file.createNewFile();
-				System.out.println("成功");
-			} catch (IOException e) {
-				System.out.println("文件创建出错");
-			}
+			file.mkdir();
+			System.out.println("成功");
 		}
 		File newfile = new File(path += "\\test.txt");
 		try {
