1c8afe0ece8bf402833f657e5bbd87b6ad7a14be
RYX <RYX18273144554@outlook.com>
2023-05-11 11:10:33 +0800

    day28

--- S1/S1 day28/src/test3/MyFrame.java
+++ S1/S1 day28/src/test3/MyFrame.java
@@ -1,7 +1,11 @@
 package test3;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.html.ImageView;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class MyFrame extends JFrame {
 	JButton[] btn=new JButton[6];
@@ -13,6 +17,14 @@ public class MyFrame extends JFrame {
 			new Color(134, 215, 80),
 			new Color(0, 60, 175)
 	};
+	String[] imgPath={
+			"/1.png",
+			"/2.png",
+			"/3.png",
+			"/4.png",
+			"/5.png",
+			"/6.png"
+	};
 	String[] text={"按钮1","按钮2","按钮3","按钮4","按钮5","按钮6"};
 	MyFrame(){
 		setTitle("流式布局");
@@ -21,7 +33,14 @@ public class MyFrame extends JFrame {
 		setLayout(new GridLayout(3,2));
 		for (int i = 0; i < btn.length; i++) {
 			btn[i]=new JButton(text[i]);
-			btn[i].setBackground(colors[i]);
+			Image img= null;
+			try {
+				img = ImageIO.read(Objects.requireNonNull(this.getClass().getResource(imgPath[i])));
+				img.getScaledInstance(390,180,Image.SCALE_DEFAULT);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			btn[i].setIcon(new ImageIcon(img));
 			add(btn[i]);
 		};
 		setSize(800,600);
